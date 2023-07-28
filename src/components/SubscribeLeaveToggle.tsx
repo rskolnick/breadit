bc7db60f1c8f9643cfbebe7906153acdae6fe9cb
Ryan Skolnick <rhskolnick@gmail.com>
2023-07-28 12:39:23 -0400

    Can subscribe unsub from communities

---
+++ src/components/SubscribeLeaveToggle.tsx
@@ -0,0 +1,122 @@
+'use client';
+
+import { FC, startTransition } from 'react';
+import { Button } from './ui/Button';
+import { useMutation } from '@tanstack/react-query';
+import { SubscribeToSubredditPayload } from '@/lib/validators/subreddit';
+import axios, { AxiosError } from 'axios';
+import { useCustomToast } from '@/hooks/use-custom-toast';
+import { toast } from '@/hooks/use-toast';
+import { useRouter } from 'next/navigation';
+
+interface SubscribeLeaveToggleProps {
+    subredditId: string;
+    subredditName: string;
+    isSubscribed: boolean;
+}
+
+const SubscribeLeaveToggle: FC<SubscribeLeaveToggleProps> = ({
+    subredditId,
+    subredditName,
+    isSubscribed,
+}) => {
+    const { loginToast } = useCustomToast();
+    const router = useRouter();
+    const { mutate: subscribe, isLoading: isSubLoading } = useMutation({
+        mutationFn: async () => {
+            const payload: SubscribeToSubredditPayload = {
+                subredditId,
+            };
+
+            const { data } = await axios.post(
+                '/api/subreddit/subscribe',
+                payload
+            );
+
+            return data as string;
+        },
+        onError: (err) => {
+            if (err instanceof AxiosError) {
+                if (err.response?.status === 401) {
+                    return loginToast();
+                }
+            }
+
+            return toast({
+                title: 'There was a problem',
+                description: 'Something went wrong, please try again.',
+                variant: 'destructive',
+            });
+        },
+        onSuccess: () => {
+            startTransition(() => {
+                router.refresh();
+            });
+
+            return toast({
+                title: 'Subscribed!',
+                description: `You are now subscribed to r/${subredditName}`,
+                variant: 'default',
+            });
+        },
+    });
+
+    const { mutate: unsubscribe, isLoading: isUnsubLoading } = useMutation({
+        mutationFn: async () => {
+            const payload: SubscribeToSubredditPayload = {
+                subredditId,
+            };
+
+            const { data } = await axios.post(
+                '/api/subreddit/unsubscribe',
+                payload
+            );
+
+            return data as string;
+        },
+        onError: (err) => {
+            if (err instanceof AxiosError) {
+                if (err.response?.status === 401) {
+                    return loginToast();
+                }
+            }
+
+            return toast({
+                title: 'There was a problem',
+                description: 'Something went wrong, please try again.',
+                variant: 'destructive',
+            });
+        },
+        onSuccess: () => {
+            startTransition(() => {
+                router.refresh();
+            });
+
+            return toast({
+                title: 'Unsubscribed!',
+                description: `You unsubscribed from r/${subredditName}`,
+                variant: 'default',
+            });
+        },
+    });
+
+    return isSubscribed ? (
+        <Button
+            onClick={() => unsubscribe()}
+            isLoading={isUnsubLoading}
+            className="w-full mt-1 mb-4"
+        >
+            Leave Community
+        </Button>
+    ) : (
+        <Button
+            onClick={() => subscribe()}
+            isLoading={isSubLoading}
+            className="w-full mt-1 mb-4"
+        >
+            Join To Post
+        </Button>
+    );
+};
+
+export default SubscribeLeaveToggle;
